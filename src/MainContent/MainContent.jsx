f51b1c41d42ac28104b779195dbe014db5aa9d9a
Ira <nikulochkina.irina@yandex.ru>
2023-10-15 19:22:21 +0300

    отрендерила пиццы из json, добавила куда надо active

--- src/MainContent/MainContent.jsx
+++ src/MainContent/MainContent.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import PizzaBlock from '../PizzaBlock/PizzaBlock';
 import styles from './MainContent.module.scss';
-
+import pizzas from './../assets/pizzas.json';
 
 const MainContent = () => {
   return (
     <div>
       <h2 className={styles.content__title}>Все пиццы</h2>
       <div className={styles.content__items}>
-        <PizzaBlock title={'Чизбургер'} price={556}/>
-        <PizzaBlock title={'Чизбургер'} price={556}/>
-        <PizzaBlock title={'Чизбургер'} price={556}/>
-        <PizzaBlock title={'Чизбургер'} price={556}/>
-        <PizzaBlock title={'Маргарита'} price={322}/>
-        <PizzaBlock title={'Чизбургер'} price={556}/>
+        {pizzas.map((pizza) => (
+          <PizzaBlock
+            {...pizza}
+          />
+        ))}
       </div>
     </div>
   );
