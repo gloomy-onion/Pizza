ebf353e6b3b82a00b72116325880f4ef7c3da167
Ira <nikulochkina.irina@yandex.ru>
2023-11-07 17:19:53 +0300

    начали делать редакс, сделала фильтр все сломав

---
+++ src/Search/Search.jsx
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react';
+import styles from './Search.module.scss';
+import searchIcon from './../img/searchIcon.svg';
+import {SearchContext} from '../App';
+
+const Search = () => {
+  const {searchValue, setSearchValue} = useContext(SearchContext)
+
+  return (
+    <div className={styles.root}>
+      <img src={searchIcon} className={styles.icon} alt={'Search Icon'}/>
+      <input value={searchValue} onChange={(event) => setSearchValue(event.target.value)}
+             placeholder={'Поиск пиццы...'}/>
+      {searchValue && (<button onClick={() => setSearchValue('')} className={styles.clearBtn}/>)}
+    </div>
+  );
+};
+
+export default Search;
